a7420aa30ba952df5254722003299e84d32eaf4f
kotoozi <u548026g@ecs.osaka-u.ac.jp>
2019-12-04 16:29:25 +0900

    It can analyze project's javafile

--- src/methodmodifiercheck/Main.java
+++ src/methodmodifiercheck/Main.java
@@ -1,5 +1,6 @@
 package methodmodifiercheck;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,16 +15,15 @@ public class Main {
 
 	private static final String PACKAGE_PATH = "src/";
 	private static final String JAVA_PACKAGE = "methodmodifiercheck/";
-	private static final String JAVA_SOURCE = "Main.java";
+//	private static final String JAVA_SOURCE = "Main.java";
 
-	public static void main(final String[] args) {
 
-		String filepath = PACKAGE_PATH + JAVA_PACKAGE + JAVA_SOURCE;
+	private static void MyVisitor(String filePath) {
 
 		List<String> lines = null;
 		// ファイル内部の文字列を読み込み
 		try {
-			lines = Files.readAllLines(Paths.get(filepath),StandardCharsets.ISO_8859_1);
+			lines = Files.readAllLines(Paths.get(filePath),StandardCharsets.ISO_8859_1);
 		}
 		// 読み込みの際に何らかのエラーが生じたときは何もせずにプログラムを終了
 		catch (Exception e) {
@@ -40,5 +40,18 @@ public class Main {
 
 		MethodVisitor methodVisitor = new MethodVisitor();
 		unit.accept(methodVisitor);
+	}
+
+	public static void main(final String[] args) {
+		String filePath = PACKAGE_PATH + JAVA_PACKAGE;
+
+	    File dir = new File(filePath);
+	    File[] list = dir.listFiles();
+	    for(int i=0; i<list.length; i++) {
+	      if(list[i].getName().contains(".java")) {
+	        System.out.println(list[i].getName());
+	        MyVisitor(filePath + list[i].getName());
+	      }
+	    }
 	}
 }
